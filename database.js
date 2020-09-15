cc6614b61db309651b996bef6e47c4fc54265358
Thibaut <thibautcuchet@hotmail.com>
2020-09-15 16:01:30 +0200

    first commit

--- database.js
+++ database.js
@@ -1,4 +1,7 @@
 const { Client } = require("pg");
+const jwt = require("jsonwebtoken");
+
+const secret = "jjk2OD6T1et6ZRZ1RDaPnvaoyADhwopS5I7NbmMQvFI=";
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -18,8 +21,22 @@ function getTop() {
     "SELECT game, COUNT(user_id) as vote FROM votes GROUP BY game ORDER BY vote DESC LIMIT 5",
     (err, res) => {
       console.log(err, res);
+      const channels = [...new Set(res.rows.map((i) => i.channel))];
+      console.log(channels);
     }
   );
+}
+
+function sendMessage(channel) {
+  let payload = {
+    exp: Date.now() + 3600,
+    channel_id: `${channel}`,
+    pubsub_perms: {
+      send: ["broadcast"],
+    },
+  };
+  let token = jwt.sign(payload, secret);
+  console.log(token);
 }
 
 module.exports = { getTop };
--- index.js
+++ index.js
@@ -5,9 +5,4 @@ const app = express();
 
 setInterval(() => getTop(), 60000);
 
-app.get("/top", (req, res) => {
-  getTop();
-  res.status(200).send("Yep");
-});
-
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
